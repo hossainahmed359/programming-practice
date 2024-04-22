32ea400fe3fa81279ceee84d3c13ddc561f5f461
Hossain-Ahmed-Madani <hossain@echologyx.com>
2024-04-23 01:10:55 +0600

    add linked list

---
+++ dsa/linked-list/linked-list.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+node1 = Node(1)
+node2 = Node(5)
+node3 = Node(10)
+node4 = Node(99)
+
+node1.next = node2
+node2.next = node3
+node3.next = node4
+
+
+current_node = node1
+
+while current_node:
+    print(current_node.data, end=" -> ")
+    current_node = current_node.next
+
+print('null')
