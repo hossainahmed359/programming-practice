e8c6a7a523dfa8b8a536d780a3ff6a03c1012129
Hossain Ahmed Madani <70832248+hossainahmed359@users.noreply.github.com>
2024-06-12 19:39:05 +0600

    Update arr-sorting.c
    Add SELECTION SORT

--- arr-sorting.c
+++ arr-sorting.c
@@ -33,3 +33,60 @@ int main() {
 
     return 0;
 }
+
+
+// ======= SELECTION SORT =======
+
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int arr[] = {15, 7, 3, 10, 5, 11, 9};
+    int len = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i = 0; i < len - 1; i++) {
+        int min_index = i;
+        
+        for(int j = i + 1; j < len; j++) {
+            if (arr[j] < arr[min_index]){
+                min_index = j;
+            }
+        }
+
+        int temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
+    }
+
+    for(int i = 0; i < len; i++) {
+        printf("%d ",arr[i]);
+    }
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
