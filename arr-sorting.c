3413c5382bba3591f0d7d67112b31bf27c148b7d
Hossain Ahmed Madani <70832248+hossainahmed359@users.noreply.github.com>
2024-06-12 19:27:45 +0600

    Create arr-sorting.c
    Add BUBBLE SORT

---
+++ arr-sorting.c
@@ -0,0 +1,35 @@
+// ======= BUBBLE SORT =======
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int arr[] = {15, 7, 3, 10, 5, 11, 9};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    
+    // printf("%d \n", len);
+    
+    for(int i = 0; i < len - 1; i ++) {
+        int swapped = false;
+        
+        for(int j = i + 1; j < len; j++) {
+            if(arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                swapped = true;
+            }
+        }
+        
+        if(swapped == false) {
+            break;
+        }
+        
+    }
+    
+    for(int i = 0; i < len ; i ++){
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
